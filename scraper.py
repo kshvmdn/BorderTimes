f5d6a88ee85ac1c020f86364eeef87f0ec1db432
Kashav Madan <kshvmdn@gmail.com>
2016-01-01 01:04:42 -0500

    Added scraping for commercial/non-commercial wait times

--- scraper.py
+++ scraper.py
@@ -1,5 +1,4 @@
 import requests, json
-import pprint
 from bs4 import BeautifulSoup as BS
 
 URL = 'http://travel.gc.ca/travelling/border-times-us'
@@ -15,27 +14,28 @@ def request():
 
 def parse(response):
     soup = BS(response.text, 'html.parser')
-    borders = {}
+    wait_times = {}
     if soup.find('table'):
         for tr in soup.find('table').find_all('tr', class_='font-small'):
-            border = tr.find_all('td')
-            port_name = border[0].find('strong').string
-            port_info = [br.next_sibling for br in border[0].find_all('br')]
+            port = tr.find_all('td')
+            port_name = port[0].find('strong').string
+            port_info = [br.next_sibling for br in port[0].find_all('br')]
             port_cities, update_time = port_info[0].split('/'), port_info[1].strip('Last updated : ')
-            borders[port_name] = {
+            commercial_wait, noncommerical_wait = [x.string for x in port[1:]]
+            wait_times[port_name] = {
                 'port_info': {
                     'port_name': port_name,
                     'from': port_cities[0],
                     'to': port_cities[1]
                 },
-                'last_updated': update_time
+                'last_updated': update_time,
+                'wait_times': {
+                    'commercial': commercial_wait,
+                    'non-commercial': non_commercial_wait
+                }
             }
-            # for time in border[1:]:
-            #     print(time)
-            # print('---')
-
-    # pprint.pprint(borders)
-
+    return wait_times
 
 if __name__ == '__main__':
-    scrape()
+    wait = scrape()
+    print(json.dumps(wait, indent=4))
